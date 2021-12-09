efcb056975052a170d433140a820747d0a1e6f27
Plasnid <stuart@sackler.ca>
2021-12-09 17:20:02 -0500

    added questions

---
+++ script.js
@@ -0,0 +1,29 @@
+/**
+ * ! Section 1 - External Data - 40 points
+ * * Create a json file with 3 objects as entries
+ * * Each object needs a title and a description of a task
+ * * Use Fetch and DOM Manipulation to load the data and turn it into a list.  
+ * * Ensure that all screen contents are built dynamically use DOM manipulation
+ */
+
+/**
+ * ! Section 2 - ES6 Classes - 30 points
+ * * Build an ES6 class representing a notebook
+ * * Have the class take in parameters for owner, colour of cover, and number of pages
+ * * Have a method in the class for taking in a note and storing it in an array
+ * * Have a method in the class for retrieving a note by its position in the array and writing it to screen in a p tag 
+ * 
+ * * make an instance of the class
+ * * add 3 notes
+ * * retrieve the second note using your method
+ */
+
+/**
+ * ! Section 3 - Bind Call and Apply - 30 points
+ * * Create 3 objects, each one representing a book.  For each object have a title, author, and genre
+ * 
+ * * Create a function for returning a phrase in the following format:  
+ * * "I have just read __title__ from __author__ and it is a __genre__ book!"
+ * 
+ * * Use apply for the first object, call for the second, and bind for the third to run your function that returns a phrase.
+ */
